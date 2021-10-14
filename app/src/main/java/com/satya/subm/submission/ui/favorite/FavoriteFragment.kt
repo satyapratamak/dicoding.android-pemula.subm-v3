ad9b7ec27d1392f22593bcd6497ffab3757b0519
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-10-14 15:30:07 +0700

    COMMIT :Movie Model, item_movie, fragment_movie.xml, fragment_favorite.xml DONE

---
+++ app/src/main/java/com/satya/subm/submission/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,7 @@
+package com.satya.subm.submission.ui.favorite
+
+import androidx.fragment.app.Fragment
+import com.satya.subm.submission.R
+
+class FavoriteFragment : Fragment(R.layout.fragment_favorite){
+}
---
+++ app/src/main/java/com/satya/subm/submission/ui/movie/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.satya.subm.submission.ui.movie
+
+import androidx.fragment.app.Fragment
+import com.satya.subm.submission.R
+
+class MovieFragment : Fragment (R.layout.fragment_movie){
+}
